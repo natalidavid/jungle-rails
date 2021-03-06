98104d1ee255bd73b7834c90ce0ba166d69095f4
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-06 22:28:43 +0000

    Fix money display bug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,15 @@
 class ProductsController < ApplicationController
 
   def index
+    #model
     @products = Product.all.order(created_at: :desc)
+    #view
+    #render: index --this is implicit
   end
 
   def show
     @product = Product.find params[:id]
   end
-
 end
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'products#index'
+  #get '/' => 'products#index' --is the same as above
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
