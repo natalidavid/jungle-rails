255b854cd5133075a485c0593f96111f038196ef
Natalie Davidovic <natalija.david@hotmail.com>
2021-03-08 03:50:13 +0000

    Fix admin categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,28 +1,7 @@
-# class CategoriesController < ApplicationController
+class CategoriesController < ApplicationController
 
-#   def show
-#     @category = Category.find(params[:id])
-#     @products = @category.products.order(created_at: :desc)
-#   end
-# end
-
-class Admin::CategoriesController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
-  def index
-    @categories = Category.order(:name).all
-  end
-
-  def new
-    @category = Category.new
-  end
-
-  def create 
-    @category = Category.new(params.require(:category).permit(:name))
-
-    if @category.save
-      redirect_to [:admin, :categories], notice: 'Category created!'
-    else
-      render :new
-    end
+  def show
+    @category = Category.find(params[:id])
+    @products = @category.products.order(created_at: :desc)
   end
 end
